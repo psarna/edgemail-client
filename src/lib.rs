d77dc502eb454f225f247c08d65da6015d31eee1
Piotr Sarna <sarna@scylladb.com>
2023-03-20 09:53:57 +0100

    beautify

--- src/lib.rs
+++ src/lib.rs
@@ -23,64 +23,56 @@ fn prepare(v: &Value) -> String {
 }
 
 fn inbox_to_html(result: QueryResult) -> String {
-    let mut html = "<table style=\"border: 1px solid\">".to_string();
+    let mut html = "<table class=\"table-hover\" style=\"border: 1px solid\">".to_string();
     match result {
         QueryResult::Error((msg, _)) => return format!("Error: {msg}"),
         QueryResult::Success((result, _)) => {
             for column in &result.columns {
-                if column != "id" {
+                if column != "id" && column != "data" {
                     html += &format!("<th style=\"border: 1px solid\">{column}</th>");
                 }
             }
             for row in result.rows {
                 let id = &row.cells["id"];
                 html += &format!(
-                    "<tr style=\"border: 1px solid\" onclick=\"window.location='./inbox/{id}'\">"
+                    "<tr style=\"border: 1px solid\" onclick=\"document.getElementById('datapanel').value = document.getElementById('data{id}').value\">"
                 );
                 for column in &result.columns {
-                    if column != "id" {
+                    if column != "id" && column != "data" {
                         html += &format!("<td>{}</td>", prepare(&row.cells[column]));
+                    } else if column == "data" {
+                        html += &format!(
+                            "<textarea id=\"data{id}\" style=\"display:none\">{}</textarea>",
+                            prepare(&row.cells[column])
+                        );
                     }
                 }
                 html += "</tr>";
             }
         }
     };
-    html += "</table>";
+    html += "</table><textarea id=\"datapanel\" style=\"height: 50%; width: 80%; margin: auto\"></textarea>";
     html
 }
 
-fn msg_to_html(result: QueryResult) -> String {
-    let msg_body = match result {
-        QueryResult::Error((msg, _)) => return format!("Error: {msg}"),
-        QueryResult::Success((result, _)) => {
-            result.rows.first().map(|row| row.cells["data"].to_string())
-        }
-    };
-    msg_body.unwrap_or_else(|| "Error: no rows found".to_string())
-}
-
-async fn serve_inbox(db: &impl libsql_client::Connection) -> anyhow::Result<String> {
+async fn serve_inbox(db: &impl libsql_client::Connection, id: &str) -> anyhow::Result<String> {
+    let canonical_id = format!("<{}@idont.date>", id);
     let response = db
-        .execute("SELECT rowid as id, date, sender, recipients FROM mail ORDER BY rowid DESC")
+        .execute(Statement::with_params("SELECT rowid as id, date, sender, recipients, data FROM mail WHERE recipients = ? ORDER BY rowid DESC", params!(canonical_id)))
         .await?;
     let table = inbox_to_html(response);
-    let style =
-        "<link rel=\"stylesheet\" href=\"https://unpkg.com/papercss@1.9.1/dist/paper.min.css\"/>";
-    let intro = "<h3>sorry@idont.date</h3><p>Subscribe to any e-mail in the domain @idont.date and receive it here!</p><br>";
-    let footer = "<footer>Made by <a href=\"https://bio.sarna.dev\">sarna</a>, powered by <a href=\"https://chiselstrike.com\">Turso</a></footer>";
-    let html = format!("{style}{intro}{table}<br>{footer}");
-    Ok(html)
-}
-
-async fn serve_msg(db: &impl libsql_client::Connection, id: i64) -> anyhow::Result<String> {
-    let response = db
-        .execute(Statement::with_params(
-            "SELECT date, sender, recipients, data FROM mail WHERE rowid = ?",
-            params!(id),
-        ))
-        .await?;
-    Ok(msg_to_html(response))
+    Ok(format!(
+        r#"
+    <link rel="stylesheet" href="https://unpkg.com/papercss@1.9.1/dist/paper.min.css"/>
+    <div style="margin:auto; width:50%">
+    <h3>sorry@idont.date</h3><h4>{id}'s inbox:</h4><br>
+    </div>
+    {table}
+    <br>
+    <div style="margin:auto; width:50%">
+    <footer>Made by <a href=\"https://bio.sarna.dev\">sarna</a>, powered by <a href=\"https://chiselstrike.com\">Turso</a></footer>
+    "#
+    ))
 }
 
 #[event(fetch)]
@@ -91,19 +83,6 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
     let router = Router::new();
 
     router
-        .get_async("/", |_req, ctx| async move {
-            let db = match libsql_client::workers::Connection::connect_from_ctx(&ctx) {
-                Ok(db) => db,
-                Err(e) => {
-                    console_log!("Error {e}");
-                    return Response::from_html(format!("Error establishing connection: {e}"));
-                }
-            };
-            match serve_inbox(&db).await {
-                Ok(html) => Response::from_html(html),
-                Err(e) => Err(Error::from(format!("{e}"))),
-            }
-        })
         .get_async("/inbox/:id", |_req, ctx| async move {
             let db = match libsql_client::workers::Connection::connect_from_ctx(&ctx) {
                 Ok(db) => db,
@@ -112,14 +91,30 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
                     return Response::from_html(format!("Error establishing connection: {e}"));
                 }
             };
-            let id: i64 = match ctx.param("id").and_then(|id| id.parse::<i64>().ok()) {
+            let id: String = match ctx.param("id").and_then(|id| id.parse::<String>().ok()) {
                 Some(id) => id,
-                None => return Response::from_html("Missing message id: /inbox/<X>"),
+                None => return Response::from_html("Missing inbox id: /inbox/<X>"),
             };
-            match serve_msg(&db, id).await {
+            match serve_inbox(&db, &id).await {
                 Ok(html) => Response::from_html(html),
                 Err(e) => Err(Error::from(format!("{e}"))),
             }
+        })
+        .get("/", |_req, _ctx| {
+            Response::from_html(r#"
+            <link rel="stylesheet" href="https://unpkg.com/papercss@1.9.1/dist/paper.min.css"/>
+            <div style="margin:auto; width:50%">
+            <h3>sorry@idont.date</h3>
+            <p>Temporary e-mail service.<br>Pick a username of your choice and go to your temporary inbox.<br>All inboxes are public.<br>Old e-mails get automatically deleted.</p>
+            <p>Choose your username:</p>
+            <form action="javascript:window.location.href= './inbox/' + (document.getElementById('inbox').value || 'test1')">
+              <input type="text" id="inbox" name="inbox" placeholder="test1">
+              <button type="button" onclick="window.location.href= './inbox/' + (document.getElementById('inbox').value || 'test1')">Go to Inbox</button>
+            </form>
+            <br>
+            <footer>Made by <a href=\"https://bio.sarna.dev\">sarna</a>, powered by <a href=\"https://chiselstrike.com\">Turso</a></footer>
+            </div>
+            "#)
         })
         .get("/worker-version", |_, ctx| {
             let version = ctx.var("WORKERS_RS_VERSION")?.to_string();
