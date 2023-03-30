c597d2f4986850c3759abf3e7fdacce3c82df478
Piotr Sarna <sarna@chiselstrike.com>
2023-03-30 13:14:58 +0200

    pseudorandom names

---
+++ login.js
@@ -0,0 +1,67 @@
+const adjectives = [
+  "majestic",
+  "fierce",
+  "cuddly",
+  "graceful",
+  "hard-working",
+  "sleek",
+  "wild",
+  "sly",
+  "persistent",
+  "regal",
+  "elegant",
+  "patient",
+  "dignified",
+  "intelligent",
+  "curious",
+  "swift",
+  "mighty",
+  "commanding",
+  "soaring"
+];
+
+const animals = [ 
+  "lynx",
+  "wolf",
+  "bear",
+  "bison",
+  "deer",
+  "beaver",
+  "polecat",
+  "boar",
+  "elk",
+  "fox",
+  "badger",
+  "eagle",
+  "stork",
+  "crane",
+  "heron",
+  "cormorant",
+  "swan",
+  "raven",
+  "magpie",
+  "buzzard",
+  "kite",
+  "eagle",
+  "stork",
+  "osprey",
+  "eagle",
+  "kestrel"
+];
+
+function pick(a) {
+  return a[Math.floor(Math.random()*a.length)];   
+}
+
+function load_name() {
+    return pick(adjectives) + "_" + pick(animals) + "_" + Math.floor(2023*Math.random())
+}
+
+document.getElementById('inbox').placeholder = load_name()
+
+function go_to_inbox() {
+  const inbox = document.getElementById('inbox');
+  window.location.href= './inbox.html?user=' + (inbox.value || inbox.placeholder)
+}
+
+document.getElementById('inbox_button').onclick = go_to_inbox;
