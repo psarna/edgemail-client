226d6e2ed3bce94e64c79aba2ffb105c47f4d7c4
Piotr Sarna <sarna@chiselstrike.com>
2023-03-31 12:46:52 +0200

    More encoding

--- inbox.js
+++ inbox.js
@@ -23,28 +23,49 @@ req.send(JSON.stringify({ statements: [{ q: "SELECT date, sender, recipients, da
 // Some of these rules are heavily inspired by https://www.npmjs.com/package/quoted-printable:
 // FIXME: proper sanitizer should read the encoding from the headers or deduce it,
 // and then apply it accordingly.
+
 function sanitize(s) {
-    const position = s.indexOf('<html') || s.indexOf('<HTML') || s.indexOf('\r\n\r\n');
     return s
-        .substring(position)
-        .replaceAll('=E2=80=8A', '')
-        .replaceAll('=E2=80=8B', '')
-        .replaceAll('=E2=80=8C', '')
-        .replaceAll('=C2=A0', '<br>')
-        .replaceAll('=E2=80=99', "'")
-        .replaceAll(/[\t\x20]$/gm, '')
-        // Remove hard line breaks preceded by `=`. Proper `Quoted-Printable`-
-        // encoded data only contains CRLF line  endings, but for compatibility
-        // reasons we support separate CR and LF too.
-        .replaceAll(/=(?:\r\n?|\n|$)/g, '')
-        // Decode escape sequences of the form `=XX` where `XX` is any
-        // combination of two hexidecimal digits. For optimal compatibility,
-        // lowercase hexadecimal digits are supported as well. See
-        // https://tools.ietf.org/html/rfc2045#section-6.7, note 1.
-        .replaceAll(/=([a-fA-F0-9]{2})/g, function (_match, target) {
-            var codePoint = parseInt(target, 16);
-            return String.fromCharCode(codePoint);
-        });
+    .replaceAll('=E2=80=8A', '')
+    .replaceAll('=E2=80=8B', '')
+    .replaceAll('=E2=80=8C', '')
+    .replaceAll('=C2=A0', '<br>')
+    .replaceAll('=E2=80=99', "'")
+    .replaceAll(/[\t\x20]$/gm, '')
+    // Remove hard line breaks preceded by `=`. Proper `Quoted-Printable`-
+    // encoded data only contains CRLF line  endings, but for compatibility
+    // reasons we support separate CR and LF too.
+    .replaceAll(/=(?:\r\n?|\n|$)/g, '')
+    // Decode escape sequences of the form `=XX` where `XX` is any
+    // combination of two hexidecimal digits. For optimal compatibility,
+    // lowercase hexadecimal digits are supported as well. See
+    // https://tools.ietf.org/html/rfc2045#section-6.7, note 1.
+    .replaceAll(/=([a-fA-F0-9]{2})/g, function (_match, target) {
+        var codePoint = parseInt(target, 16);
+        return String.fromCharCode(codePoint);
+    });
+}
+
+function parse(email) {
+    console.log(email)
+    const subject_position = email.indexOf('Subject: ') || email.indexOf('SUBJECT: ');
+    let subject = email.substring(subject_position + 9, email.indexOf('\r\n', subject_position));
+    if (subject.toLowerCase().startsWith("=?utf-8?")) {
+        subject = sanitize(subject.substring(10));
+    }
+
+    const from_position = email.indexOf('From: ') || email.indexOf('FROM: ');
+    let from = email.substring(from_position + 6, email.indexOf('\r\n', from_position))
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;");
+    if (from.toLowerCase().startsWith("=?utf-8?")) {
+        from = sanitize(from.substring(10));
+    }
+
+    const body_position = email.indexOf('<body') || email.indexOf('<BODY') || email.indexOf('\r\n\r\n');
+    const body = sanitize(email.substring(body_position))
+
+    return [from, subject, body];
 }
 
 function createTable(data) {
@@ -58,9 +79,9 @@ function createTable(data) {
     const th2 = document.createElement('th');
     const th3 = document.createElement('th');
     th1.style.border = th2.style.border = th3.style.border = '1px solid';
-    th1.innerHTML = data.columns[0];
-    th2.innerHTML = data.columns[1];
-    th3.innerHTML = data.columns[2];
+    th1.innerHTML = "from";
+    th2.innerHTML = "subject";
+    th3.innerHTML = "date";
     tr.appendChild(th1);
     tr.appendChild(th2);
     tr.appendChild(th3);
@@ -72,14 +93,15 @@ function createTable(data) {
         const td2 = document.createElement('td');
         const td3 = document.createElement('td');
         td1.style.border = td2.style.border = td3.style.border = '1px solid';
-        td1.innerHTML = new Date(row[0]).toLocaleString();
-        td2.innerHTML = row[1].slice(1, -1);
-        td3.innerHTML = row[2].slice(1, -1);
+        const [from, subject, body] = parse(row[3]);
+        td1.innerHTML = from || row[1].slice(1, -1);
+        td2.innerHTML = subject || "[no subject]";
+        td3.innerHTML = new Date(row[0]).toLocaleString();
         tr.appendChild(td1);
         tr.appendChild(td2);
         tr.appendChild(td3);
         tr.onclick = () => {
-            document.getElementById('datapanel').innerHTML = sanitize(row[3]);
+            document.getElementById('datapanel').innerHTML = body;
         }
         tbody.appendChild(tr);
     }
